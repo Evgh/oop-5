ace194d07d8c001b54c885b96a016f81c6dde7ab
Evgh <kasperovich.official@gmail.com>
2020-10-28 19:02:24 +0300

    First task

--- Program.cs
+++ Program.cs
@@ -2,11 +2,89 @@
 
 namespace oop_5
 {
+
+    interface IProduct
+    {
+        bool ToBuy()
+        {
+            Console.WriteLine("Товар куплен");
+            return true;
+        }
+
+        bool ToSell()
+        {
+            Console.WriteLine("Товар продан");
+            return true;
+        }
+    }
+
+    abstract class Tech 
+    {
+        protected bool isRunning;
+        public Tech()
+        {
+            isRunning = false;
+        }
+
+        public bool turnOn()
+        {
+            isRunning = true;
+            return true;
+        }
+
+        public bool turnOff()
+        {
+            isRunning = false;
+            return true;
+        }
+
+        public virtual bool isItRunning()
+        {
+            return isRunning;
+        }
+    }
+
+    abstract class TypingTech : Tech
+    {
+        public abstract bool InputText();
+
+    }
+
+    sealed class Computer : TypingTech, IProduct
+    {
+        public override bool InputText()
+        {
+            Console.WriteLine("Набирается текст на клавиатуре");
+            return true;
+        } 
+    }
+
+    sealed class Tablet : TypingTech, IProduct 
+    {
+        public override bool InputText()
+        {
+            Console.WriteLine("Набирается текст на экранной клавиатуре");
+            return true;
+        }
+    }
+
+    sealed class Scaner : Tech, IProduct
+    {
+
+    }
+
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Computer thing = new Computer();
+            thing.turnOn();
+
+            Console.WriteLine(thing.isItRunning());
+
         }
     }
 }
