aa2897ca3a450d8a4e4e8acd07c1a1f09e628901
Evgh <kasperovich.official@gmail.com>
2020-10-28 20:43:39 +0300

    First+second+third+foth

--- Program.cs
+++ Program.cs
@@ -4,41 +4,44 @@ namespace oop_5
 {
 
     interface IProduct
-    {
-        bool ToBuy()
+    { 
+        void ToBuy()
         {
-            Console.WriteLine("Товар куплен");
-            return true;
+            Console.WriteLine("Товар куплен.");
         }
 
-        bool ToSell()
+        void ToSell()
         {
-            Console.WriteLine("Товар продан");
-            return true;
+            Console.WriteLine("Товар продан.");
+        }
+
+        void DoThing()
+        {
+            Console.WriteLine("Товар делает штуку по-интерфейсовски.");
         }
     }
 
     abstract class Tech 
     {
         protected bool isRunning;
-        public Tech()
+        protected Tech()
         {
             isRunning = false;
         }
 
-        public bool turnOn()
+        public bool TurnOn()
         {
             isRunning = true;
             return true;
         }
 
-        public bool turnOff()
+        public bool TurnOff()
         {
             isRunning = false;
             return true;
         }
 
-        public virtual bool isItRunning()
+        public virtual bool IsItRunning()
         {
             return isRunning;
         }
@@ -47,33 +50,117 @@ namespace oop_5
     abstract class TypingTech : Tech
     {
         public abstract bool InputText();
-
     }
+
+
+    sealed class Scaner : Tech, IProduct
+    {   
+        public override bool IsItRunning()
+        {
+            if (base.isRunning)
+            {
+                Console.WriteLine("Сканер сканирует.");
+            }
+            else
+            {
+                Console.WriteLine("Сканер не сканирует.");
+            }
+            return base.isRunning;
+        }
+
+        // Штуки.
+        public void DoThing()
+        {
+            Console.WriteLine("Сканер делает штуку по-сканерски");
+        }
+        void IProduct.DoThing()
+        {
+            Console.WriteLine("Сканер делает штуку по-интерфейсовски");
+        }
+    }
+
 
     sealed class Computer : TypingTech, IProduct
     {
+        public override bool IsItRunning()
+        {
+            if (base.isRunning)
+            {
+                Console.WriteLine("Компьютер компьютерирует");
+            }
+            else
+            {
+                Console.WriteLine("Компьютер не компьютерирует");
+            }
+            return base.isRunning;
+        }
         public override bool InputText()
         {
             Console.WriteLine("Набирается текст на клавиатуре");
             return true;
-        } 
+        }
+
+
+
+
+        // Штуки.
+        public void DoThing()
+        {
+            Console.WriteLine("Компьютер делает штуку по-компьютерски");
+        }
+        void IProduct.DoThing()
+        {
+            Console.WriteLine("Компьютер делает штуку по-интерфейсовски");
+        }
     }
 
     sealed class Tablet : TypingTech, IProduct 
     {
+        public override bool IsItRunning()
+        {
+            if (base.isRunning)
+            {
+                Console.WriteLine("Планшет планшетирует");
+            }
+            else
+            {
+                Console.WriteLine("Планшет не планшетирует");
+            }
+            return base.isRunning;
+        }
+
+
         public override bool InputText()
         {
-            Console.WriteLine("Набирается текст на экранной клавиатуре");
+            Console.WriteLine("Набирается текст пальцами");
             return true;
         }
+
+        // Штуки.
+        public void DoThing()
+        {
+            Console.WriteLine("Планшет делает штуку по-планшетски");
+        }
+        void IProduct.DoThing()
+        {
+            Console.WriteLine("Планшет делает штуку по-интерфейсовски");
+        }
+
+        // Переопределение методов обджекта.
+        public override int GetHashCode()
+        {
+            Random rr = new Random();
+            return rr.Next();
+        }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public override string ToString()
+        {
+            return $"Сканер{GetHashCode()}";
+        }
     }
-
-    sealed class Scaner : Tech, IProduct
-    {
-
-    }
-
-
 
 
     class Program
@@ -81,9 +168,12 @@ namespace oop_5
         static void Main(string[] args)
         {
             Computer thing = new Computer();
-            thing.turnOn();
+            thing.TurnOn();
 
-            Console.WriteLine(thing.isItRunning());
+            thing.DoThing();
+            ((IProduct)thing).DoThing();
+
+            Console.WriteLine(thing.IsItRunning());
 
         }
     }
